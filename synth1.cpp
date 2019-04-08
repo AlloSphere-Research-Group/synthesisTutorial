3466801fe8f576c15645b1aa422be1254f84ab77
Andres Cabrera <andres@atari>
2019-04-08 14:31:36 -0700

    Updates for Spring'19

--- synth1.cpp
+++ synth1.cpp
@@ -1,4 +1,4 @@
-/*    Gamma - Generic processing library
+﻿/*    Gamma - Generic processing library
     See COPYRIGHT file for authors and license information
 
     Example:
@@ -11,158 +11,173 @@
 
 #include "Gamma/Gamma.h"
 
-#include "al/core/io/al_AudioIO.hpp"
-#include "al/util/ui/al_SynthSequencer.hpp"
+#include "al/core/app/al_App.hpp"
+#include "al/core/graphics/al_Shapes.hpp"
+#include "al/util/ui/al_Parameter.hpp"
+#include "al/util/scene/al_PolySynth.hpp"
+#include "al/util/scene/al_SynthSequencer.hpp"
+#include "al/util/ui/al_ControlGUI.hpp"
 
-using namespace gam;
+//using namespace gam;
 using namespace al;
 
-
+// This is the same SineEnv class defined in graphics/synth1.cpp
+// It inclludes drawing code
 class SineEnv : public SynthVoice {
 public:
 
-    SineEnv()
-    {
-        set (6.5, 60, 0.3, 1, 2);
+    // Unit generators
+    gam::Pan<> mPan;
+    gam::Sine<> mOsc;
+    gam::Env<3> mAmpEnv;
+    gam::EnvFollow<> mEnvFollow;  // envelope follower to connect audio output to graphics
+
+    // Additional members
+    Mesh mMesh;
+
+    // Initialize voice. This function will nly be called once per voice
+    virtual void init() {
+
+        // Intialize envelope
         mAmpEnv.curve(0); // make segments lines
         mAmpEnv.levels(0,1,1,0);
-    }
+        mAmpEnv.sustainPoint(2); // Make point 2 sustain until a release is issued
 
-    // Note parameters
-    SineEnv& freq(float v){ mOsc.freq(v); return *this; }
-    SineEnv& amp(float v){ mAmp=v; return *this; }
-    SineEnv& attack(float v){
-        mAmpEnv.lengths()[0] = v;
-        return *this;
-    }
-    SineEnv& decay(float v){
-        mAmpEnv.lengths()[2] = v;
-        return *this;
-    }
+        // We have the mesh be a sphere
+        addDisc(mMesh, 1.0, 30);
 
-    SineEnv& dur(float v){ mDur=v; return *this; }
-
-    SineEnv& pan(float v){ mPan.pos(v); return *this; }
-
-    SineEnv& set(
-        float a, float b, float c, float d, float e, float f=0
-    ){
-        return dur(a).freq(b).amp(c).attack(d).decay(e).pan(f);
+        createInternalTriggerParameter("amplitude", 0.3, 0.0, 1.0);
+        createInternalTriggerParameter("frequency", 60, 20, 5000);
+        createInternalTriggerParameter("attackTime", 1.0, 0.01, 3.0);
+        createInternalTriggerParameter("releaseTime", 3.0, 0.1, 10.0);
+        createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
     }
 
     //
     virtual void onProcess(AudioIOData& io) override {
+
+        // Parameters will update values once per audio callback
+        mOsc.freq(getInternalParameterValue("frequency"));
+        mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
+        mAmpEnv.lengths()[2] = getInternalParameterValue("releaseTime");
+        mPan.pos(getInternalParameterValue("pan"));
         while(io()){
-            float s1 = mOsc() * mAmpEnv() * mAmp;
+            float s1 = mOsc() * mAmpEnv() * getInternalParameterValue("amplitude");
             float s2;
             mEnvFollow(s1);
             mPan(s1, s1,s2);
             io.out(0) += s1;
             io.out(1) += s2;
         }
-        //if(mAmpEnv.done()) free();
+        // We need to let the synth know that this voice is done
+        // by calling the free(). This takes the voice out of the
+        // rendering chain
         if(mAmpEnv.done() && (mEnvFollow.value() < 0.001f)) free();
     }
 
     virtual void onProcess(Graphics &g) {
-
+        float frequency = getInternalParameterValue("frequency");
+        float amplitude = getInternalParameterValue("amplitude");
+        g.pushMatrix();
+        g.translate(frequency/200 - 3,  amplitude, -8);
+        g.scale(1- amplitude, amplitude, 1);
+        g.color(mEnvFollow.value(), frequency/1000, mEnvFollow.value()* 10, 0.4);
+        g.draw(mMesh);
+        g.popMatrix();
     }
 
     virtual void onTriggerOn() override {
-        mAmpEnv.totalLength(mDur, 1);
         mAmpEnv.reset();
     }
 
-protected:
-
-    float mAmp;
-    float mDur;
-    Pan<> mPan;
-    Sine<> mOsc;
-    Env<3> mAmpEnv;
-    EnvFollow<> mEnvFollow;
+    virtual void onTriggerOff() override {
+        mAmpEnv.release();
+    }
 };
 
 
+// We make an app.
+class MyApp : public App
+{
+public:
 
-int main(){
-    SynthSequencer s;
+    // GUI manager for SineEnv voices
+    // The name provided determines the name of the directory
+    // where the presets and sequences are stored
+    SynthGUIManager<SineEnv> synthManager {"synth1"};
 
-    //s.add( 0).set(2.5,  60, 0.3, .1, .2);
-    s.add<SineEnv>( 0  ).set(3.5, 260, 0.3, .011, .2);
-    s.add<SineEnv>( 0  ).set(3.5, 510, 0.3, .011, .2);
-    s.add<SineEnv>( 3.5).set(3.5, 233, 0.3, .011, .2);
-    s.add<SineEnv>( 3.5).set(3.5, 340, 0.3, .011, .2);
-    s.add<SineEnv>( 3.5).set(7.5, 710, 0.3, 1, 2);
-    //s.add<SineEnv>( 6.5).set(1.5,  60, 0.3, 1, 2);
-    s.add<SineEnv>( 6.5).set(1.5,  60, 0.3, .1, .2);
-    s.add<SineEnv>( 8.0).set(3.5, 230, 0.3, 1, 2);
-    s.add<SineEnv>(11.5).set(7.5, 450, 0.3, 1, 2);
-    s.add<SineEnv>(11.5).set(3.0, 240, 0.3, 1, 2);
-    s.add<SineEnv>(11.5).set(1.0,  90, 0.3, .1, .2);
-    s.add<SineEnv>(12.5).set(2.0, 120, 0.3, .1,.2);
-    s.add<SineEnv>(14.5).set(4.5, 380, 0.3, 1, 2);
-    s.add<SineEnv>(14.5).set(3.0, 240, 0.3, 1, 2);
-    //s.add<SineEnv>(17.5).set(1.5, 110, 0.3, 1, 2);
-    s.add<SineEnv>(17.5).set(1.5, 110, 0.3, .1, .2);
-    s.add<SineEnv>(19.0).set(8.0, 940, 0.3, 1, 2);
-    s.add<SineEnv>(19.0).set(8.0, 780, 0.3, 1, 2);
-    s.add<SineEnv>(19.0).set(8.0, 640, 0.3, 1, 2);
-    s.add<SineEnv>(27.0).freq(233).amp(0.1);
-    s.add<SineEnv>(27.0).freq(234).amp(0.1);
-    s.add<SineEnv>(27.0).freq(235).amp(0.1);
-    s.add<SineEnv>(27.0).freq(232).amp(0.1);
-    s.add<SineEnv>(27.0).freq(231).amp(0.1);
-    s.add<SineEnv>(27.0).freq(230).amp(0.1);
-    s.add<SineEnv>(31.0).freq(329.6).amp(0.1);
-    s.add<SineEnv>(31.0).freq(330.6).amp(0.1);
-    s.add<SineEnv>(31.0).freq(331.6).amp(0.1);
-    s.add<SineEnv>(31.0).freq(334.6).amp(0.1);
-    s.add<SineEnv>(31.0).freq(335.6).amp(0.1);
-    s.add<SineEnv>(31.0).freq(337.6).amp(0.1);
-    s.add<SineEnv>(35.0).freq(233).amp(0.1);
-    s.add<SineEnv>(35.0).freq(234).amp(0.1);
-    s.add<SineEnv>(35.0).freq(235).amp(0.1);
-    s.add<SineEnv>(35.0).freq(232).amp(0.1);
-    s.add<SineEnv>(35.0).freq(231).amp(0.1);
-    s.add<SineEnv>(35.0).freq(230).amp(0.1);
-    s.add<SineEnv>(39.0).freq(440).amp(0.1);
-    s.add<SineEnv>(39.0).freq(441).amp(0.1);
-    s.add<SineEnv>(39.0).freq(442).amp(0.1);
-    s.add<SineEnv>(39.0).freq(443).amp(0.1);
-    s.add<SineEnv>(39.0).freq(445).amp(0.1);
-    s.add<SineEnv>(39.0).freq(446).amp(0.1);
-    s.add<SineEnv>(45.0).set(8.0, 940, 0.1, 1, 2);
-    s.add<SineEnv>(45.0).set(8.0, 941, 0.1, 1, 2);
-    s.add<SineEnv>(45.0).set(8.0, 942, 0.1, 1, 2);
-    s.add<SineEnv>(47.0).set(8.0, 780, 0.1, 1, 2);
-    s.add<SineEnv>(47.0).set(8.0, 781, 0.1, 1, 2);
-    s.add<SineEnv>(47.0).set(8.0, 782, 0.1, 1, 2);
-    s.add<SineEnv>(49.0).set(8.0, 640, 0.1, 1, 2);
-    s.add<SineEnv>(49.0).set(8.0, 641, 0.1, 1, 2);
-    s.add<SineEnv>(49.0).set(8.0, 642, 0.1, 1, 2);
-    s.add<SineEnv>(51.5).set(7.5, 450, 0.3, 1, 2);
-    s.add<SineEnv>(51.5).set(3.0, 240, 0.3, 1, 2);
-    //s.add<SineEnv>(51.5).set(1.0,  90, 0.3, 1, 2);
-    //s.add<SineEnv>(52.5).set(2.0, 120, 0.3, 1, 2);
-    s.add<SineEnv>(51.5).set(1.0,  90, 0.3, .1, .2);
-    s.add<SineEnv>(52.5).set(2.0, 120, 0.3, .1, .2);
-    s.add<SineEnv>(54.5).set(4.5, 380, 0.3, 1, 2);
-    s.add<SineEnv>(54.5).set(3.0, 240, 0.3, 1, 2);
-    //s.add<SineEnv>(57.5).set(1.5, 110, 0.3, 1, 2);
-    s.add<SineEnv>(57.5).set(1.5, 110, 0.3, .1, .2);
-    s.add<SineEnv>( 68  ).set(6.5, 260, 0.3, 1, 2);
-    s.add<SineEnv>( 68  ).set(3.5, 510, 0.3, 1, 2);
-    s.add<SineEnv>( 65.5).set(3.0, 233, 0.3, 1, 2);
-    s.add<SineEnv>( 65.5).set(4.5, 340, 0.3, 1, 2);
-    s.add<SineEnv>( 65.5).set(7.5, 710, 0.3, 1, 2);
-    //s.add<SineEnv>( 62.5).set(1.5,  60, 0.3, 1, 2);
-    s.add<SineEnv>( 62.5).set(1.5,  60, 0.3, .1, .2);
-    s.add<SineEnv>( 60.0).set(3.5, 230, 0.3, 1, 2);
+    // This function is called right after the window is created
+    // It provides a grphics context to initialize ParameterGUI
+    // It's also a good place to put things that should
+    // happen once at startup.
+    virtual void onCreate() override {
+        ParameterGUI::initialize();
 
-    AudioIO io;
-    io.initWithDefaults(s.audioCB, &s, true, false);
-    Domain::master().spu(io.framesPerSecond());
-    io.start();
-    printf("\nPress 'enter' to quit...\n"); getchar();
+        // Play example sequence. Comment this line to start from scratch
+        synthManager.synthSequencer().playSequence("synth1.synthSequence");
+        synthManager.synthRecorder().verbose(true);
+    }
+
+    // The audio callback function. Called when audio hardware requires data
+    virtual void onSound(AudioIOData &io) override {
+        synthManager.render(io); // Render audio
+    }
+
+    // The graphics callback function.
+    virtual void onDraw(Graphics &g) override {
+        g.clear();
+        synthManager.render(g);
+
+        // Draw GUI
+        ParameterGUI::beginDraw();
+        synthManager.drawSynthControlPanel();
+        ParameterGUI::endDraw();
+    }
+
+    // Whenever a key is pressed, this function is called
+    virtual void onKeyDown(Keyboard const& k) override {
+        if (ParameterGUI::usingKeyboard()) { // Ignore keys if GUI is using keyboard
+            return;
+        }
+        if (k.shift()) {
+            // If shift pressed then keyboard sets preset
+            int presetNumber = asciiToIndex(k.key());
+            synthManager.recallPreset(presetNumber);
+        } else {
+            // Otherwise trigger note for polyphonic synth
+            int midiNote = asciiToMIDI(k.key());
+            if (midiNote > 0) {
+              synthManager.voice()->setInternalParameterValue("frequency", ::pow(2.f, (midiNote - 69.f)/12.f) * 432.f);
+              synthManager.triggerOn(midiNote);
+            }
+        }
+    }
+
+    // Whenever a key is released this function is called
+    virtual void onKeyUp(Keyboard const& k) override {
+        int midiNote = asciiToMIDI(k.key());
+        if (midiNote > 0) {
+            synthManager.triggerOff(midiNote);
+        }
+    }
+
+    void onExit() override {
+        ParameterGUI::cleanup();
+    }
+};
+
+
+int main(){    // Create app instance
+    MyApp app;
+
+    app.navControl().active(false); // Disable navigation via keyboard, since we will be using keyboard for note triggering
+
+    // Set up audio
+    app.initAudio(48000., 256, 2, 0);
+    // Set sampling rate for Gamma objects from app's audio
+    gam::sampleRate(app.audioIO().framesPerSecond());
+    app.audioIO().print();
+
+    app.start();
+    return 0;
 }
