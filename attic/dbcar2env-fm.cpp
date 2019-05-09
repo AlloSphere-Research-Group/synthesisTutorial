2af06e765b6a017006f9fc48e1706dca6681502d
Andres Cabrera <andres@atari>
2019-05-09 11:00:30 -0700

    Old instruments working put in attic/

---
+++ attic/dbcar2env-fm.cpp
@@ -0,0 +1,177 @@
+/*    Gamma - Generic processing library
+    See COPYRIGHT file for authors and license information
+
+    Example:
+    Description:
+*/
+
+#include <cstdio>               // for printing to stdout
+#define GAMMA_H_INC_ALL         // define this to include all header files
+#define GAMMA_H_NO_IO           // define this to avoid bringing AudioIO from Gamma
+
+#include "Gamma/Gamma.h"
+
+#include "al/core/io/al_AudioIO.hpp"
+#include "al/util/scene/al_SynthSequencer.hpp"
+
+using namespace gam;
+using namespace al;
+
+
+class DBCFM : public SynthVoice {
+public:
+
+    DBCFM()
+        : mAmp(1), mAmp2(1), mDur(2),
+          mCarFrq(440), mCarMul(1), mModMul(1), mModAmt(50) , mCarMul2(1), mModMul2(1), mModAmt2(50)
+    {
+        set( 10, 262, 0.5, 0.5, 0.1,5.1, 5.1,0.1, 0.75,0.75, 0.01,7,5, 1,1.0007,1,1.414, 0);
+    }
+
+    DBCFM& freq(float v){ mCarFrq=v; return *this; }
+    DBCFM& amp(float v){ mAmp=v; return *this; }
+    DBCFM& amp2(float v){ mAmp2=v; return *this; }
+
+    DBCFM& attack(float v){
+        mAmpEnv.lengths()[0] = v;
+        mModEnv.lengths()[0] = v;
+        return *this;
+    }
+    DBCFM& decay(float v){
+        mAmpEnv.lengths()[2] = v;
+        mModEnv.lengths()[2] = v;
+        return *this;
+    }
+    DBCFM& attack2(float v){
+        mAmpEnv2.lengths()[0] = v;
+        mModEnv.lengths()[0] = v;
+        return *this;
+    }
+    DBCFM& decay2(float v){
+        mAmpEnv2.lengths()[2] = v;
+        mModEnv.lengths()[2] = v;
+        return *this;
+    }
+
+    DBCFM& sus(float v){
+        mAmpEnv.levels()[2] = 1;
+        return *this;
+    }
+
+    DBCFM& sus2(float v){
+        mAmpEnv2.levels()[2] = 1;
+        return *this;
+    }
+    DBCFM& dur(float v){ mDur=v; return *this; }
+
+    DBCFM& pan(float v){ mPan.pos(v); return *this; }
+
+    DBCFM& carMul(float v){ mCarMul=v; return *this; }
+    DBCFM& modMul(float v){ mModMul=v; return *this; }
+    DBCFM& modAmt(float v){ mModAmt=v; return *this; }
+
+    DBCFM& carMul2(float v){ mCarMul2=v; return *this; }
+    DBCFM& modMul2(float v){ mModMul2=v; return *this; }
+    DBCFM& modAmt2(float v){ mModAmt2=v; return *this; }
+
+    DBCFM& idx1(float v){ mModEnv.levels()[0]=v; return *this; }
+    DBCFM& idx2(float v){ mModEnv.levels()[1]=v; return *this; }
+    DBCFM& idx3(float v){ mModEnv.levels()[2]=v; return *this; }
+
+    DBCFM& set(float a, float b, float c, float d, float e, float f,
+            float g, float h, float i, float j, float k, float l, float m, float n, float o, float p, float q, float r){
+        return
+            dur(a).freq(b).amp(c).amp2(d).attack(e).decay(f).attack2(g).decay2(h).sus(i).sus2(j)
+        .idx1(k).idx2(l).idx3(m).carMul(n).modMul(o).carMul2(p).modMul2(q).pan(r);
+    }
+
+    virtual void onProcess(AudioIOData& io) override {
+
+        float modFreq = mCarFrq * mModMul;
+        float mod2Freq = mCarFrq * mModMul2;
+
+        while(io()){
+            car.freq(mCarFrq*mCarMul + mod()*mModEnv()*modFreq);
+            car2.freq(mCarFrq*mCarMul2 + mod2()*mModEnv()*mod2Freq);
+            float s1 = ((car()* mAmpEnv()* mAmp)   + (car2()* mAmpEnv2()* mAmp2));
+            float s2;
+            mEnvFollow(s1);
+            mEnvFollow2(s1);
+            mPan(s1, s1,s2);
+            io.out(0) += s1;
+            io.out(1) += s2;
+        }
+
+        if(mAmpEnv.done() && (mEnvFollow.value() < 0.001)) free();
+        if(mAmpEnv2.done() && (mEnvFollow2.value() < 0.001)) free();
+    }
+
+    virtual void onTriggerOn() override {
+
+        float modFreq = mCarFrq * mModMul;
+        float mod2Freq = mCarFrq * mModMul2;
+        mod.freq(modFreq);
+        mod2.freq(mod2Freq);
+
+        mAmpEnv.totalLength(mDur, 1);
+        mAmpEnv2.totalLength(mDur, 1);
+        //mModEnv.lengths()[1] = mAmpEnv.lengths()[1];
+        //mModEnv.lengths()[1] = mAmpEnv2.lengths()[1];
+        mModEnv.lengths()[1] = mAmpEnv.lengths()[3];
+        mModEnv.lengths()[1] = mAmpEnv2.lengths()[3];
+
+        mAmpEnv.reset();
+        mAmpEnv2.reset();
+        mModEnv.reset();
+    }
+
+protected:
+    // general synth parameters
+    //float mPitch; // implicit
+    float mAmp;
+    float mAmp2;
+    float mDur;
+    Pan<> mPan;
+
+    // specific parameters
+    float mCarFrq;        // carrier frequency
+    float mCarMul;        // carrier frequency multiplier
+    float mModMul;        // modulator frequency multiplier
+    float mModAmt;        // frequency modulation amount
+    float mCarMul2;        // carrier frequency multiplier
+    float mModMul2;        // modulator frequency multiplier
+    float mModAmt2;        // frequency modulation amount
+
+    Sine<> car, car2, mod, mod2;    // carrier, modulator sine oscillators
+    Env<3> mAmpEnv;
+    Env<3> mAmpEnv2;
+    Env<3> mModEnv;
+    EnvFollow<> mEnvFollow;
+    EnvFollow<> mEnvFollow2;
+};
+
+
+int main(){
+
+    SynthSequencer s;
+    s.add<DBCFM>( 0).set( 10, 262, 0.5, 0.5, 0.1, 5.1, 5.1, 0.1, 0.75, 0.75, 0.01, 7.0, 5.0,1.0, 1.0007,1,1.414, 1);
+    s.add<DBCFM>(10).set( 10, 262, 0.5, 0.5, 5.1, 0.1, 0.1, 5.1, 0.81, 0.81, 1.7, 1.4, 1.1, 1.0, 1.998, 1.001, 1.414, 0);
+    s.add<DBCFM>( 20).freq(220);
+    s.add<DBCFM>( 30).set( 10, 262, 0.5, 0.5, 5.1, 0.1, 0.1, 5.1, 0.81, 0.81, 0.01,7,5, 1,1.0007, 2, 1.007, 0);
+    s.add<DBCFM>( 40).set( 10, 262, 0.5, 0.5, 0.1, 3.1, 0.1, 3.1, 0.81, 0.81, 0.01,7,5, 1,1.0007, 1.0007, 5.008, -1);
+
+
+    AudioIO io;
+    io.initWithDefaults(s.audioCB, &s, true, false);
+    Domain::master().spu(io.framesPerSecond());
+    io.start();
+    printf("\nPress 'enter' or Ctrl-C to quit...\n");
+    while (io.isRunning()) {
+      auto c = getchar();
+      if (c != -1) {
+        std::cout << c <<std::endl;
+        io.stop();
+      }
+    }
+    return 0;
+}
---
+++ attic/synth1.cpp
@@ -0,0 +1,176 @@
+/*    Gamma - Generic processing library
+    See COPYRIGHT file for authors and license information
+
+    Example:
+    Description:
+*/
+
+#include <cstdio>               // for printing to stdout
+#define GAMMA_H_INC_ALL         // define this to include all header files
+#define GAMMA_H_NO_IO           // define this to avoid bringing AudioIO from Gamma
+
+#include "Gamma/Gamma.h"
+
+#include "al/core/io/al_AudioIO.hpp"
+#include "al/util/scene/al_SynthSequencer.hpp"
+
+using namespace gam;
+using namespace al;
+
+
+class SineEnv : public SynthVoice {
+public:
+
+    SineEnv()
+    {
+        set (6.5, 60, 0.3, 1, 2);
+        mAmpEnv.curve(0); // make segments lines
+        mAmpEnv.levels(0,1,1,0);
+    }
+
+    // Note parameters
+    SineEnv& freq(float v){ mOsc.freq(v); return *this; }
+    SineEnv& amp(float v){ mAmp=v; return *this; }
+    SineEnv& attack(float v){
+        mAmpEnv.lengths()[0] = v;
+        return *this;
+    }
+    SineEnv& decay(float v){
+        mAmpEnv.lengths()[2] = v;
+        return *this;
+    }
+
+    SineEnv& dur(float v){ mDur=v; return *this; }
+
+    SineEnv& pan(float v){ mPan.pos(v); return *this; }
+
+    SineEnv& set(
+        float a, float b, float c, float d, float e, float f=0
+    ){
+        return dur(a).freq(b).amp(c).attack(d).decay(e).pan(f);
+    }
+
+    //
+    virtual void onProcess(AudioIOData& io) override {
+        while(io()){
+            float s1 = mOsc() * mAmpEnv() * mAmp;
+            float s2;
+            mEnvFollow(s1);
+            mPan(s1, s1,s2);
+            io.out(0) += s1;
+            io.out(1) += s2;
+        }
+        //if(mAmpEnv.done()) free();
+        if(mAmpEnv.done() && (mEnvFollow.value() < 0.001f)) free();
+    }
+
+    virtual void onProcess(Graphics &g) {
+
+    }
+
+    virtual void onTriggerOn() override {
+        mAmpEnv.totalLength(mDur, 1);
+        mAmpEnv.reset();
+    }
+
+protected:
+
+    float mAmp;
+    float mDur;
+    Pan<> mPan;
+    Sine<> mOsc;
+    Env<3> mAmpEnv;
+    EnvFollow<> mEnvFollow;
+};
+
+
+
+int main(){
+    SynthSequencer s;
+
+    //s.add( 0).set(2.5,  60, 0.3, .1, .2);
+    s.add<SineEnv>( 0  ).set(3.5, 260, 0.3, .011, .2);
+    s.add<SineEnv>( 0  ).set(3.5, 510, 0.3, .011, .2);
+    s.add<SineEnv>( 3.5).set(3.5, 233, 0.3, .011, .2);
+    s.add<SineEnv>( 3.5).set(3.5, 340, 0.3, .011, .2);
+    s.add<SineEnv>( 3.5).set(7.5, 710, 0.3, 1, 2);
+    //s.add<SineEnv>( 6.5).set(1.5,  60, 0.3, 1, 2);
+    s.add<SineEnv>( 6.5).set(1.5,  60, 0.3, .1, .2);
+    s.add<SineEnv>( 8.0).set(3.5, 230, 0.3, 1, 2);
+    s.add<SineEnv>(11.5).set(7.5, 450, 0.3, 1, 2);
+    s.add<SineEnv>(11.5).set(3.0, 240, 0.3, 1, 2);
+    s.add<SineEnv>(11.5).set(1.0,  90, 0.3, .1, .2);
+    s.add<SineEnv>(12.5).set(2.0, 120, 0.3, .1,.2);
+    s.add<SineEnv>(14.5).set(4.5, 380, 0.3, 1, 2);
+    s.add<SineEnv>(14.5).set(3.0, 240, 0.3, 1, 2);
+    //s.add<SineEnv>(17.5).set(1.5, 110, 0.3, 1, 2);
+    s.add<SineEnv>(17.5).set(1.5, 110, 0.3, .1, .2);
+    s.add<SineEnv>(19.0).set(8.0, 940, 0.3, 1, 2);
+    s.add<SineEnv>(19.0).set(8.0, 780, 0.3, 1, 2);
+    s.add<SineEnv>(19.0).set(8.0, 640, 0.3, 1, 2);
+    s.add<SineEnv>(27.0).freq(233).amp(0.1);
+    s.add<SineEnv>(27.0).freq(234).amp(0.1);
+    s.add<SineEnv>(27.0).freq(235).amp(0.1);
+    s.add<SineEnv>(27.0).freq(232).amp(0.1);
+    s.add<SineEnv>(27.0).freq(231).amp(0.1);
+    s.add<SineEnv>(27.0).freq(230).amp(0.1);
+    s.add<SineEnv>(31.0).freq(329.6).amp(0.1);
+    s.add<SineEnv>(31.0).freq(330.6).amp(0.1);
+    s.add<SineEnv>(31.0).freq(331.6).amp(0.1);
+    s.add<SineEnv>(31.0).freq(334.6).amp(0.1);
+    s.add<SineEnv>(31.0).freq(335.6).amp(0.1);
+    s.add<SineEnv>(31.0).freq(337.6).amp(0.1);
+    s.add<SineEnv>(35.0).freq(233).amp(0.1);
+    s.add<SineEnv>(35.0).freq(234).amp(0.1);
+    s.add<SineEnv>(35.0).freq(235).amp(0.1);
+    s.add<SineEnv>(35.0).freq(232).amp(0.1);
+    s.add<SineEnv>(35.0).freq(231).amp(0.1);
+    s.add<SineEnv>(35.0).freq(230).amp(0.1);
+    s.add<SineEnv>(39.0).freq(440).amp(0.1);
+    s.add<SineEnv>(39.0).freq(441).amp(0.1);
+    s.add<SineEnv>(39.0).freq(442).amp(0.1);
+    s.add<SineEnv>(39.0).freq(443).amp(0.1);
+    s.add<SineEnv>(39.0).freq(445).amp(0.1);
+    s.add<SineEnv>(39.0).freq(446).amp(0.1);
+    s.add<SineEnv>(45.0).set(8.0, 940, 0.1, 1, 2);
+    s.add<SineEnv>(45.0).set(8.0, 941, 0.1, 1, 2);
+    s.add<SineEnv>(45.0).set(8.0, 942, 0.1, 1, 2);
+    s.add<SineEnv>(47.0).set(8.0, 780, 0.1, 1, 2);
+    s.add<SineEnv>(47.0).set(8.0, 781, 0.1, 1, 2);
+    s.add<SineEnv>(47.0).set(8.0, 782, 0.1, 1, 2);
+    s.add<SineEnv>(49.0).set(8.0, 640, 0.1, 1, 2);
+    s.add<SineEnv>(49.0).set(8.0, 641, 0.1, 1, 2);
+    s.add<SineEnv>(49.0).set(8.0, 642, 0.1, 1, 2);
+    s.add<SineEnv>(51.5).set(7.5, 450, 0.3, 1, 2);
+    s.add<SineEnv>(51.5).set(3.0, 240, 0.3, 1, 2);
+    //s.add<SineEnv>(51.5).set(1.0,  90, 0.3, 1, 2);
+    //s.add<SineEnv>(52.5).set(2.0, 120, 0.3, 1, 2);
+    s.add<SineEnv>(51.5).set(1.0,  90, 0.3, .1, .2);
+    s.add<SineEnv>(52.5).set(2.0, 120, 0.3, .1, .2);
+    s.add<SineEnv>(54.5).set(4.5, 380, 0.3, 1, 2);
+    s.add<SineEnv>(54.5).set(3.0, 240, 0.3, 1, 2);
+    //s.add<SineEnv>(57.5).set(1.5, 110, 0.3, 1, 2);
+    s.add<SineEnv>(57.5).set(1.5, 110, 0.3, .1, .2);
+    s.add<SineEnv>( 68  ).set(6.5, 260, 0.3, 1, 2);
+    s.add<SineEnv>( 68  ).set(3.5, 510, 0.3, 1, 2);
+    s.add<SineEnv>( 65.5).set(3.0, 233, 0.3, 1, 2);
+    s.add<SineEnv>( 65.5).set(4.5, 340, 0.3, 1, 2);
+    s.add<SineEnv>( 65.5).set(7.5, 710, 0.3, 1, 2);
+    //s.add<SineEnv>( 62.5).set(1.5,  60, 0.3, 1, 2);
+    s.add<SineEnv>( 62.5).set(1.5,  60, 0.3, .1, .2);
+    s.add<SineEnv>( 60.0).set(3.5, 230, 0.3, 1, 2);
+
+    AudioIO io;
+    io.initWithDefaults(s.audioCB, &s, true, false);
+    Domain::master().spu(io.framesPerSecond());
+    io.start();
+    printf("\nPress 'enter' or Ctrl-C to quit...\n");
+    while (io.isRunning()) {
+      auto c = getchar();
+      if (c != -1) {
+        std::cout << c <<std::endl;
+        io.stop();
+      }
+    }
+    return 0;
+}
