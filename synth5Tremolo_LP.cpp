25e556374fd23f4f55a5c98bda10fcc24ebccf42
Andres Cabrera <andres@atari>
2019-05-13 14:19:39 -0700

    Incorporated JoAnn's work on synth5.

--- synth5Tremolo_LP.cpp
+++ synth5Tremolo_LP.cpp
@@ -19,130 +19,179 @@
 #include "al/util/scene/al_SynthSequencer.hpp"
 #include "al/util/ui/al_ControlGUI.hpp"
 
-using namespace gam;
+//using namespace gam;
 using namespace al;
 
 // tables for oscillator
 gam::ArrayPow2<float>
-tbSin(2048), tbSqr(2048), tbPls(2048), tbDin(2048);
+    tbSaw(2048), tbSqr(2048), tbImp(2048), tbSin(2048), tbPls(2048),
+    tb__1(2048), tb__2(2048), tb__3(2048), tb__4(2048);
 
 
 class OscTrm : public SynthVoice {
 public:
 
-  gam::Osc<> mTrm;
-  gam::ADSR<> mTrmEnv;
-  gam::Sine<> mOsc;
-  gam::ADSR<> mAmpEnv;
-  gam::EnvFollow<> mEnvFollow;
-  gam::Pan<> mPan;
+    // Unit generators
+    gam::Pan<> mPan;
+    gam::Sine<> mTrm;
+    gam::Osc<> mOsc;
+    gam::ADSR<> mTrmEnv;
+    //gam::Env<2> mTrmEnv;
+    gam::ADSR<> mAmpEnv;
+    gam::EnvFollow<> mEnvFollow;  // envelope follower to connect audio output to graphics
 
-  Mesh mMesh;
+    // Additional members
+    Mesh mMesh;
 
-  void init( ) override {
-    mAmpEnv.levels(0,1,1,0);
-    mTrmEnv.curve(0);
+    // Initialize voice. This function will nly be called once per voice
+    virtual void init() {
 
-    // We have the mesh be a sphere
-    addDisc(mMesh, 1.0, 30);
+        // Intialize envelope
+        mAmpEnv.curve(0); // make segments lines
+        mAmpEnv.levels(0,0.3,0.3,0); // These tables are not normalized, so scale to 0.3
+        mAmpEnv.sustainPoint(2); // Make point 2 sustain until a release is issued
+        mTrmEnv.curve(0);
 
-    createInternalTriggerParameter("frequency", 440, 10, 4000.0);
-    createInternalTriggerParameter("amplitude", 0.5, 0.0, 1.0);
-    createInternalTriggerParameter("attackTime", 0.1, 0.01, 3.0);
-    createInternalTriggerParameter("releaseTime", 0.1, 0.1, 10.0);
-    createInternalTriggerParameter("sustain", 0.75, 0.1, 1.0);
-    createInternalTriggerParameter("trmDepth", 0.5, 0.0, 1.0);
-    createInternalTriggerParameter("trm1", 4, 0.1, 15.0);
-    createInternalTriggerParameter("trm2", 8, 0.1, 15.0);
-    createInternalTriggerParameter("trmRise", 0.5, 0.1, 3.0);
-    createInternalTriggerParameter("table", 0.0, 0.0, 3.0);
-    createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
-  }
+        // We have the mesh be a sphere
+        addDisc(mMesh, 1.0, 30);
 
-  virtual void onProcess(AudioIOData& io) override {
-    mOsc.freq(getInternalParameterValue("frequency"));
-
-    float amp = getInternalParameterValue("amplitude");
-    float trmDepth = getInternalParameterValue("trmDepth");
-    while(io()){
-
-
-      mTrm.freq(mTrmEnv());
-      //float trmAmp = mAmp - mTrm()*mTrmDepth; // Replaced with line below
-      float trmAmp = (mTrm()*0.5+0.5)*trmDepth + (1-trmDepth); // Corrected
-
-      float s1 = mOsc() * mAmpEnv() * trmAmp * amp;
-      float s2;
-      mEnvFollow(s1);
-      mPan(s1, s1,s2);
-      io.out(0) += s1;
-      io.out(1) += s2;
+        createInternalTriggerParameter("amplitude", 0.1, 0.0, 1.0);
+        createInternalTriggerParameter("frequency", 60, 20, 5000);
+        createInternalTriggerParameter("attackTime", 0.1, 0.01, 3.0);
+        createInternalTriggerParameter("releaseTime", 3.0, 0.1, 10.0);
+        createInternalTriggerParameter("sustain", 0.7, 0.0, 1.0);
+        createInternalTriggerParameter("curve", 4.0, -10.0, 10.0);
+        createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
+        createInternalTriggerParameter("table", 0, 0, 8);
+        createInternalTriggerParameter("trm1", 3.5, 0.2, 20);
+        createInternalTriggerParameter("trm2", 5.8, 0.2, 20);
+        createInternalTriggerParameter("trmRise", 0.5, 0.1, 2);
+        createInternalTriggerParameter("trmDepth", 0.005, 0.0, 0.3);
     }
-    //if(mAmpEnv.done()) free();
-    if(mAmpEnv.done() && (mEnvFollow.value() < 0.001)) free();
-  }
 
-  virtual void onTriggerOn() override {
+    //
+    virtual void onProcess(AudioIOData& io) override {
+        //updateFromParameters();
+        float oscFreq = getInternalParameterValue("frequency");
+        float amp = getInternalParameterValue("amplitude");
+        float trmDepth = getInternalParameterValue("trmDepth");
+         while(io()){
 
-    mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
-    mAmpEnv.lengths()[1] = 0.001;
-    mAmpEnv.lengths()[2] = getInternalParameterValue("releaseTime");
-
-    mAmpEnv.levels()[1]= getInternalParameterValue("sustain");
-    mAmpEnv.levels()[2]= getInternalParameterValue("sustain");
-
-    mTrmEnv.levels(getInternalParameterValue("trm1"),
-                  getInternalParameterValue("trm2"),
-                   getInternalParameterValue("trm2"),
-                  getInternalParameterValue("trm1"));
-
-    mTrmEnv.lengths(getInternalParameterValue("trmRise"),
-                   0.001,
-                   getInternalParameterValue("trmRise"));
-
-    mPan.pos(getInternalParameterValue("pan"));
-
-    mAmpEnv.reset();
-    mTrmEnv.reset();
-    // Map table number to table in memory
-    switch (int(getInternalParameterValue("table"))) {
-    case 0: mTrm.source(tbSin); break;
-    case 1: mTrm.source(tbSqr); break;
-    case 2: mTrm.source(tbPls); break;
-    case 3: mTrm.source(tbDin); break;
+            mTrm.freq(mTrmEnv());
+            //float trmAmp = mAmp - mTrm()*mTrmDepth; // Replaced with line below
+            float trmAmp = (mTrm()*0.5+0.5)*trmDepth + (1-trmDepth); // Corrected
+            float s1 = mOsc() * mAmpEnv() * trmAmp * amp;
+            float s2;
+            mEnvFollow(s1);
+            mPan(s1, s1,s2);
+            io.out(0) += s1;
+            io.out(1) += s2;
+        }
+        // We need to let the synth know that this voice is done
+        // by calling the free(). This takes the voice out of the
+        // rendering chain
+        if(mAmpEnv.done() && (mEnvFollow.value() < 0.001f)) free();
     }
-  }
 
-  virtual void onTriggerOff() override {
-      mAmpEnv.triggerRelease();
-      mTrmEnv.triggerRelease();
-  }
+virtual void onProcess(Graphics &g) {
+        float frequency = getInternalParameterValue("frequency");
+        float amplitude = getInternalParameterValue("amplitude");
+        g.pushMatrix();
+        g.translate(amplitude,  amplitude, -4);
+        g.scale(frequency/200, frequency/400, 1);
+        //float scaling = trmDepth + getInternalParameterValue("trmDepth");
+        //g.scale(scaling * frequency/200, scaling * frequency/400, scaling* 1);
+        g.color(mEnvFollow.value(), frequency/1000, mEnvFollow.value()* 10, 0.4);
+        g.draw(mMesh);
+        g.popMatrix();
+    }
+
+    virtual void onTriggerOn() override {
+        updateFromParameters();
+        mAmpEnv.reset();
+        mTrmEnv.reset();
+        
+        // Map table number to table in memory
+        switch (int(getInternalParameterValue("table"))) {
+        case 0: mOsc.source(tbSaw); break;
+        case 1: mOsc.source(tbSqr); break;
+        case 2: mOsc.source(tbImp); break;
+        case 3: mOsc.source(tbSin); break;
+        case 4: mOsc.source(tbPls); break;
+        case 5: mOsc.source(tb__1); break;
+        case 6: mOsc.source(tb__2); break;
+        case 7: mOsc.source(tb__3); break;
+        case 8: mOsc.source(tb__4); break;
+        }
+    }
+
+    virtual void onTriggerOff() override {
+        mAmpEnv.triggerRelease();
+        mTrmEnv.triggerRelease();
+    }
+
+    void updateFromParameters() {
+        mOsc.freq(getInternalParameterValue("frequency"));
+        mAmpEnv.attack(getInternalParameterValue("attackTime"));
+        mAmpEnv.decay(getInternalParameterValue("attackTime"));
+        mAmpEnv.release(getInternalParameterValue("releaseTime"));
+        mAmpEnv.sustain(getInternalParameterValue("sustain"));
+        mAmpEnv.curve(getInternalParameterValue("curve"));
+        mPan.pos(getInternalParameterValue("pan"));
+        mTrmEnv.levels(getInternalParameterValue("trm1"),
+                       getInternalParameterValue("trm2"),
+                       getInternalParameterValue("trm2"),
+                       getInternalParameterValue("trm1"));
+        mTrmEnv.lengths()[0] = getInternalParameterValue("trmRise");
+        mTrmEnv.lengths()[1] = getInternalParameterValue("trmRise");
+        mTrmEnv.lengths()[3] = getInternalParameterValue("trmRise");
+    }
 };
 
 
-
+// We make an app.
 class MyApp : public App
 {
 public:
 
-  virtual void onInit( ) override {
-    addWave(tbSin, SINE);
-    addWave(tbSqr, SQUARE);
-    addWave(tbPls, IMPULSE, 4);
+    virtual void onInit( ) override {
+        gam::addSinesPow<1>(tbSaw, 9,1);
+        gam::addSinesPow<1>(tbSqr, 9,2);
+        gam::addSinesPow<0>(tbImp, 9,1);
+        gam::addSine(tbSin);
 
-    // inharmonic partials
-    {    float A[] = {1, 0.7, 0.45, 0.3, 0.15, 0.08};
-      float C[] = {10, 27, 54, 81, 108, 135};
-      addSines(tbDin, A,C,6);
+        {    float A[] = {1,1,1,1,0.7,0.5,0.3,0.1};
+            gam::addSines(tbPls, A,8);
+        }
+
+        {    float A[] = {1, 0.4, 0.65, 0.3, 0.18, 0.08};
+            float C[] = {1,4,7,11,15,18};
+            gam::addSines(tb__1, A,C,6);
+        }
+
+        // inharmonic partials
+        {    float A[] = {0.5,0.8,0.7,1,0.3,0.4,0.2,0.12};
+            float C[] = {3,4,7,8,11,12,15,16};
+            gam::addSines(tb__2, A,C,8);
+        }
+
+        // inharmonic partials
+        {    float A[] = {1, 0.7, 0.45, 0.3, 0.15, 0.08};
+            float C[] = {10, 27, 54, 81, 108, 135};
+            gam::addSines(tb__3, A,C,6);
+        }
+
+        // harmonics 20-27
+        {    float A[] = {0.2, 0.4, 0.6, 1, 0.7, 0.5, 0.3, 0.1};
+            gam::addSines(tb__4, A,8, 20);
+        }
     }
-
-  }
 
     virtual void onCreate() override {
         ParameterGUI::initialize();
 
         // Play example sequence. Comment this line to start from scratch
-//        synthManager.synthSequencer().playSequence("synth6.synthSequence");
+        synthManager.synthSequencer().playSequence("synth5.synthSequence");
         synthManager.synthRecorder().verbose(true);
     }
 
@@ -191,30 +240,24 @@ public:
         ParameterGUI::cleanup();
     }
 
+    // GUI manager for OscEnv voices
+    // The name provided determines the name of the directory
+    // where the presets and sequences are stored
     SynthGUIManager<OscTrm> synthManager {"synth5"};
 };
 
 
-//SynthSequencer s;
-//s.add<OscAM>( 0).set(5, 262, 0.5, 0.1,0.08,0.8, 0.2,0.8,0.5, 1.4870, tbSin);
-//s.add<OscAM>( 5).set(10, 262, 0.5, 0.1,0.08,0.8, 0.0,1.0,0.0, 2.0001, tbSin);
-//s.add<OscAM>(15).set(5, 262, 0.5, 0.1,0.08,0.8, 0.2,0.8,1.0, 2.0001, tbPls);
-//s.add<OscAM>(20).set(5, 262, 0.5, 0.1,0.08,0.8, 0.2,0.8,1.0, 2.0001/10., tbDin);
+int main(){    // Create app instance
+    MyApp app;
 
+    app.navControl().active(false); // Disable navigation via keyboard, since we will be using keyboard for note triggering
 
+    // Set up audio
+    app.initAudio(48000., 256, 2, 0);
+    // Set sampling rate for Gamma objects from app's audio
+    gam::sampleRate(app.audioIO().framesPerSecond());
+    app.audioIO().print();
 
-int main(){
-  MyApp app;
-
-  app.navControl().active(false); // Disable navigation via keyboard, since we will be using keyboard for note triggering
-
-  // Set up audio
-  app.initAudio(48000., 256, 2, 0);
-  // Set sampling rate for Gamma objects from app's audio
-  gam::sampleRate(app.audioIO().framesPerSecond());
-  app.audioIO().print();
-
-  app.start();
-
-  return 0;
+    app.start();
+    return 0;
 }
