748dcbfe694019d968dd9dbd1e4548144d4d6c25
ben <benedic.lee@gmail.com>
2019-05-22 19:26:03 -0700

    synth8 fixed

--- synth8.cpp
+++ synth8.cpp
@@ -22,7 +22,7 @@
 //using namespace gam;
 using namespace al;
 
-class synth8 : public SynthVoice {
+class Sub : public SynthVoice {
 public:
 
     // Unit generators
@@ -45,7 +45,7 @@ public:
         mAmpEnv.sustainPoint(2); // Make point 2 sustain until a release is issued
         mCFEnv.curve(0);
         mBWEnv.curve(0);
-
+        mOsc.harmonics(12);
         // We have the mesh be a sphere
         addDisc(mMesh, 1.0, 30);
 
@@ -123,26 +123,27 @@ public:
         mOsc.harmonics(getInternalParameterValue("hmnum"));
         mOsc.ampRatio(getInternalParameterValue("hmamp"));
         mAmpEnv.attack(getInternalParameterValue("attackTime"));
-        mAmpEnv.decay(getInternalParameterValue("attackTime"));
+    //    mAmpEnv.decay(getInternalParameterValue("attackTime"));
         mAmpEnv.release(getInternalParameterValue("releaseTime"));
-        mAmpEnv.sustain(getInternalParameterValue("sustain"));
+        mAmpEnv.levels()[1]=getInternalParameterValue("sustain");
+        mAmpEnv.levels()[2]=getInternalParameterValue("sustain");
+
         mAmpEnv.curve(getInternalParameterValue("curve"));
         mPan.pos(getInternalParameterValue("pan"));
-       // mNoise.seed(getInternalParameterValue("noise"));
         mCFEnv.levels(getInternalParameterValue("cf1"),
                        getInternalParameterValue("cf2"),
+                       getInternalParameterValue("cf2"),
                        getInternalParameterValue("cf1"));
-
-
         mCFEnv.lengths()[0] = getInternalParameterValue("cfRise");
         mCFEnv.lengths()[1] = 1 - getInternalParameterValue("cfRise");
- //       mCFEnv.lengths()[3] = getInternalParameterValue("cfRise");
+        mCFEnv.lengths()[3] = getInternalParameterValue("cfRise");
         mBWEnv.levels(getInternalParameterValue("bw1"),
+                       getInternalParameterValue("bw2"),
                        getInternalParameterValue("bw2"),
                        getInternalParameterValue("bw1"));
         mBWEnv.lengths()[0] = getInternalParameterValue("bwRise");
         mBWEnv.lengths()[1] = 1- getInternalParameterValue("bwRise");
-//        mBWEnv.lengths()[3] = getInternalParameterValue("bwRise");        
+        mBWEnv.lengths()[3] = getInternalParameterValue("bwRise");        
     }
 };
 
@@ -206,7 +207,7 @@ public:
  
     // The name provided determines the name of the directory
     // where the presets and sequences are stored
-    SynthGUIManager<synth8> synthManager {"synth8"};
+    SynthGUIManager<Sub> synthManager {"synth8"};
 };
 
 
